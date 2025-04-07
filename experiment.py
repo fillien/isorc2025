a6260d7f65e9651acc79286b498c6e1ed1dea4bd
François Illien <francois@illien.org>
2025-04-07 08:52:10 +0200

    add the static energy scenario

--- experiment.py
+++ experiment.py
@@ -7,18 +7,16 @@ for umax in ["0_3", "0_6", "0_9"]:
         os.system(f"python ./schedsim/scripts/simulate.py tasksets/data_umax{umax} {algo}")
 
 for umax in ["0_3", "0_6", "0_9"]:
-    os.system(f"python ./schedsim/scripts/energy-consumption-vs-utilization.py tasksets/data_umax{umax} energy_umax{umax}.csv")
+    os.system(f"python ./schedsim/scripts/energy-consumption-vs-utilization.py tasksets/data_umax{umax} energy_umax{umax}.csv platforms/exynos5422LITTLE.json")
+    os.system(f"python ./schedsim/scripts/energy-consumption-vs-utilization.py tasksets/data_umax{umax} energyx10_umax{umax}.csv platforms/exynos5422LITTLEx10.json")
 
 for umax in ["0_3", "0_6", "0_9"]:
     df = pd.read_csv(f"energy_umax{umax}.csv", sep=" ")
-
     x = df['util']
     grub = df['grub']
     pa = df['pa']
     ffa = df['ffa']
     csf = df['csf']
-
-    # Plot
     plt.plot(x, grub, label='GRUB')
     plt.plot(x, pa, label='GRUB Power Aware')
     plt.plot(x, ffa, label='Frequency First')
@@ -26,7 +24,25 @@ for umax in ["0_3", "0_6", "0_9"]:
     plt.xlabel('Total Utilization $U$')
     plt.ylabel('Power Consumption [W]')
     plt.legend()
-    plt.title(f"Power Consumption with umax < {umax.replace("_", ".")}")
+    plt.title(f"Power Consumption with umax < {umax.replace("_", ".")}\nScenario 1 (predominance of dynamic energy)")
     plt.grid(True)
     plt.savefig(f"plot_energy_umax{umax}.png")
+    plt.close()
+
+    df = pd.read_csv(f"energyx10_umax{umax}.csv", sep=" ")
+    x = df['util']
+    grub = df['grub']
+    pa = df['pa']
+    ffa = df['ffa']
+    csf = df['csf']
+    plt.plot(x, grub, label='GRUB')
+    plt.plot(x, pa, label='GRUB Power Aware')
+    plt.plot(x, ffa, label='Frequency First')
+    plt.plot(x, csf, label='Core State First')
+    plt.xlabel('Total Utilization $U$')
+    plt.ylabel('Power Consumption [W]')
+    plt.legend()
+    plt.title(f"Power Consumption with umax < {umax.replace("_", ".")},\nScenario 2 (predominance of static energy)")
+    plt.grid(True)
+    plt.savefig(f"plot_energyx10_umax{umax}.png")
     plt.close()
